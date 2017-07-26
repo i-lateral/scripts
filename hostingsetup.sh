09aa2ecbea22b35c8e11ab8fc4c17afeef58e9e6
Morven Lewis-Everley <morven@i-lateral.com>
2017-07-26 11:26:48 +0100

    Add hosting setup script

---
+++ hostingsetup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd ~;
+
+# First create bin folder
+echo "Creating bin dir...";
+mkdir 'bin';
+cd 'bin';
+
+# Install sake
+echo "Installing Sake...";
+wget "https://raw.githubusercontent.com/silverstripe/silverstripe-framework/3.6/sake";
+chmod +x sake;
+
+# Install sspak
+echo "Installing SSPAK...";
+wget "https://silverstripe.github.io/sspak/sspak.phar";
+chmod +x sspak.phar;
+mv sspak.phar sspak;
+
+# Now add .bash_profile
+echo "Setting up Bash Profile";
+cd ~;
+touch .bash_profile;
+echo 'PATH=$PATH:~/bin' >> .bash_profile;
