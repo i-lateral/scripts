e14cee3b187b08ce9cee92e0dba0ae3bb25304be
Morven Lewis-Everley <morven@i-lateral.com>
2017-08-01 11:20:22 +0100

    Add SS Environement creation

--- hostingsetup.sh
+++ hostingsetup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+## Basic Hosting Setup Script, add this to the home directory on the server
+## and it will download necissary commands to a bin folder in the home directory
+## setup your system path and add the default _ss_environment file.
+
 cd ~;
 
 # First create bin folder
@@ -28,3 +32,38 @@ echo "Setting up Bash Profile";
 cd ~;
 touch .bash_profile;
 echo 'PATH=$PATH:~/bin' >> .bash_profile;
+
+# Now add _ss_environment
+echo "Adding SS Environment";
+echo "What is the default domain (WITHOUT HTTP)? ";
+read defaultdomain;
+echo "What is the DB name? ";
+read dbname;
+echo "What is the DB username? ";
+read dbuser;
+echo "What is the DB password? ";
+read dbpass;
+echo "What is the SS admin email? ";
+read adminemail;
+echo "What is the SS admin password? ";
+read adminpass;
+
+touch _ss_environment.php;
+echo -e "<?php
+/* What kind of environment is this: development, test, or live (ie, production)? */
+define('SS_ENVIRONMENT_TYPE', 'live');
+
+/* Database connection */
+define('SS_DATABASE_SERVER', 'localhost');
+define('SS_DATABASE_USERNAME', '$dbuser');
+define('SS_DATABASE_PASSWORD', '$dbpass');
+define('SS_DATABASE_NAME', '$dbname');
+
+/* Command Line Access */
+global \$_FILE_TO_URL_MAPPING;
+\$_FILE_TO_URL_MAPPING['/var/www/vhosts/$defaultdomain/httpdocs'] = 'http://$defaultdomain';
+
+/* Configure a default username and password to access the CMS on all sites in this environment. */
+define('SS_DEFAULT_ADMIN_USERNAME', '$adminemail');
+define('SS_DEFAULT_ADMIN_PASSWORD', '$adminpass');" >> _ss_environment.php;
+
