c043accbefc72c4b6dfc4be1961f97499b61ed9f
Morven Lewis-Everley <morven@i-lateral.com>
2017-08-17 17:47:59 +0100

    Add additional command to clear cache folder (if set)

--- deployment.sh
+++ deployment.sh
@@ -9,3 +9,8 @@ echo  "[$DATE] Running composer and sake" > $DIR/logs/deployment.log;
 composer install --no-dev 2>&1 | tee -a $DIR/logs/deployment.log;
 php framework/cli-script.php dev/build flush=1 >> $DIR/logs/deployment.log;
 php framework/cli-script.php / flush=1;
+
+## If cache dir is present, clear it
+if [ ! -d "silverstripe-cache" ]; then
+    rm -fR silverstripe-cache/*
+fi
