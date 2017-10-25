191804a66d9103a9245b5d5bc610cc0c102e380b
Morven Lewis-Everley <morven@i-lateral.com>
2017-10-25 15:18:47 +0100

    Ensure correct permissions are set to assets

--- deployment.sh
+++ deployment.sh
@@ -11,6 +11,11 @@ php framework/cli-script.php dev/build flush=1 >> $DIR/logs/deployment.log;
 php framework/cli-script.php / flush=1;
 
 ## If cache dir is present, clear it
+echo "[$DATE] Clearing Cache";
 if [ ! -d "silverstripe-cache" ]; then
     rm -fR silverstripe-cache/*
 fi
+
+## Ensure correct permissions are set on assets
+echo "[$DATE] Setting Assets Permissions";
+chmod -R 777 assets;
